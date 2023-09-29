b3fdc2fa8c863a42d736eec84fee6762418f1891
Dexter <dexter.lente18@gmail.com>
2023-09-29 13:03:26 +0800

    admin

--- nxt/api/urls.py
+++ nxt/api/urls.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 urlpatterns = [
     # path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('login/', views.LoginAPIView.as_view(), name='login'),
-    # path('register/', views.register, name='register'),
+    path('register/', views.register, name='register'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('posts/', views.post_list.as_view(), name='posts'),
     path('profile/', views.profile, name='profile'),
