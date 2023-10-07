e1e432573023ecbb8dba82e50e4018c1f14c8283
Dexter <dexter.lente18@gmail.com>
2023-10-07 16:27:01 +0800

    navbarworks

---
+++ nxt/frontend/src/app/(navbar)/navbar.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const NavBar: React.FC = () => {
+  return (
+    <div>NaASEDASDSADSADSADASDSA NAVSvBar</div>
+  )
+}
+
+export default NavBar
--- nxt/frontend/src/app/layout.tsx
+++ nxt/frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NavBar from '@/app/(navbar)/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,8 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* todo */}
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <NavBar />
+        {children}</body>
     </html>
   )
 }
