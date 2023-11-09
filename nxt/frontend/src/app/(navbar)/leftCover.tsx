8019fdc027377548f3ba07cd8401732b404c36cd
Dexter <dexter.lente18@gmail.com>
2023-11-09 18:20:40 +0800

    article

--- nxt/frontend/src/app/(navbar)/leftCover.tsx
+++ nxt/frontend/src/app/(navbar)/leftCover.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const LeftCover = () => {
-  return <div>LeftCover</div>;
+  return (
+    <div className=" ">
+      <div className="flex justify-center text-3xl font-bold">Top Stories</div>
+      <div className=" mt-10 w-50 ml-6 w-4/5 justify-center">
+        <div className="text-[22px] font-medium text-[#121212]">
+          LOREM LOREM IPSUMSUM SAMESSAME LOREM LOREM
+        </div>
+        <div className="text-[#5a5a5a] text-[14px] border-b-2 border-solid border-[#727272] mb-8 pb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          facilisis hendrerit justo, ut maximus augue malesuada vel. Nunc non
+          laoreet diam, quis bibendum quam. Aliquam facilisis purus nec elit
+          semper feugiat. Integer sit amet scelerisque nunc, sed interdum ipsum.
+          Quisque commodo nec odio eu mattis. Proin nec nibh ut mauris
+          vestibulum pharetra at ut ex. Cras risus neque, facilisis vel rhoncus
+          a, pulvinar sed lacus.
+        </div>
+        <div className="text-[22px] font-medium text-[#121212]">
+          LOREM LOREM IPSUMSUM SAMESSAME LOREM LOREM
+        </div>
+        <div className="text-[#5a5a5a] text-[14px] border-b-2 border-solid border-[#727272] mb-8 pb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          facilisis hendrerit justo, ut maximus augue malesuada vel. Nunc non
+          laoreet diam, quis bibendum quam. Aliquam facilisis purus nec elit
+          semper feugiat. Integer sit amet scelerisque nunc, sed interdum ipsum.
+          Quisque commodo nec odio eu mattis. Proin nec nibh ut mauris
+          vestibulum pharetra at ut ex. Cras risus neque, facilisis vel rhoncus
+          a, pulvinar sed lacus.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftCover;
