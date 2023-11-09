bdc825e724c1cfaf1167b1e00ce81261137de359
Dexter <dexter.lente18@gmail.com>
2023-11-09 15:28:55 +0800

    new

--- nxt/frontend/src/app/(navbar)/cover.tsx
+++ nxt/frontend/src/app/(navbar)/cover.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React from "react";
 import RightCover from "./rightCover";
+import LeftCover from "./leftCover";
 
 const Cover = () => {
   return (
-    <div>
+    <div className="grid grid-cols-2 mt-10">
+      <LeftCover />
       <RightCover />
     </div>
   );
---
+++ nxt/frontend/src/app/(navbar)/rightCover.tsx
@@ -0,0 +1,67 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+
+const RightCover = () => {
+  const slides = [
+    {
+      url: "../1.png",
+      width: 30,
+      height: 30,
+    },
+    {
+      url: "../2.png",
+      width: 30,
+      height: 30,
+    },
+    {
+      url: "../3.png",
+      width: 30,
+      height: 30,
+    },
+  ];
+  const [currentIndex, setCurrentIndex] = useState<any>(0);
+  const autoScroll = true;
+  let slideInterval: any;
+  let intervalTime = 1500;
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  useEffect(() => {
+    if (autoScroll) {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      slideInterval = setInterval(nextSlide, intervalTime);
+    }
+    return () => clearInterval(slideInterval);
+  }, [currentIndex]);
+
+  return (
+    <div className="mb-10">
+      <div className="h-[585px] hidden lg:block">
+        <div className="max-w-[1280] w-11/12 m-auto py-6 h-screen px-4 relative group">
+          <div
+            style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+            className="w-full h-4/6 rounded-2xl bg-center bg-contain  duration-500"
+          ></div>
+          <div className="flex top-4 justify-center">
+            {slides.map((slide, slideIndex) => (
+              <div
+                key={slideIndex}
+                className={`slide ${
+                  slideIndex === currentIndex ? "" : ""
+                } transition-opacity duration-1000 ease-in-out`}
+                style={{ opacity: slideIndex === currentIndex ? 1 : 0.3 }}
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightCover;
