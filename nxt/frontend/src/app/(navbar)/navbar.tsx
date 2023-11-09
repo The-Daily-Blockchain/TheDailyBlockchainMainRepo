cc238b4817f6c7a01b0d0e8dce5a684f2e4b64e4
Dexter <dexter.lente18@gmail.com>
2023-11-09 13:55:04 +0800

    Next headerSuze

--- nxt/frontend/src/app/(navbar)/navbar.tsx
+++ nxt/frontend/src/app/(navbar)/navbar.tsx
@@ -6,17 +6,17 @@ import Image from "next/image";
 const NavBar: React.FC = () => {
   return (
     <div className="bg-[#fff]">
-      <div className="flex border-b-2 border-gray-300">
+      <div className="flex border-b-2 border-[#ebebeb]">
         <div className="mx-auto">
           <Image
             src="/Daily.png"
             alt="Daily Blockchain Ph"
-            width={600}
-            height={300}
+            width={700}
+            height={400}
           />
         </div>
       </div>
-      <div className="flex justify-center border-double border-b-4 bg-[#FFFFFF] space-x-16 py-3 text-[12px]">
+      <div className="flex justify-center border-double border-b-4 border-[#000] bg-[#FFFFFF] space-x-16 py-3 text-[12px]">
         <div> About</div>
         <div> Top News</div>
         <div> Crypto101</div>
