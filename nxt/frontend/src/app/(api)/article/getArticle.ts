468b7b654bc8d0d8ef60c1c7a3d7c82117229a68
Dexter <dexter.lente18@gmail.com>
2023-11-17 14:45:59 +0800

    TODO

---
+++ nxt/frontend/src/app/(api)/article/getArticle.ts
@@ -0,0 +1,25 @@
+// pages/api/data-endpoint.js
+
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    // Make a GET request to your backend API
+    const response = await axios.get("https://your-backend-api.com/data");
+
+    // Extract the data from the response
+    const data = response.data;
+
+    // Return the data as JSON
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+// TODO
