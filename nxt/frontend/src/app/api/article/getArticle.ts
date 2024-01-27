5bbd8d8dcb8903c8c879b97514d7f79dd439eb42
Dexter <dexter.lente18@gmail.com>
2024-01-27 11:59:36 +0800

    fixapi

--- nxt/frontend/src/app/api/article/getArticle.ts
+++ nxt/frontend/src/app/api/article/getArticle.ts
@@ -1,5 +1,6 @@
 // pages/api/data-endpoint.js
 
+import { API_URL } from "@/app/config";
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -8,18 +9,12 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    // Make a GET request to your backend API
-    const response = await axios.get("https://your-backend-api.com/data");
-
-    // Extract the data from the response
+    const response = await axios.get(`${API_URL}/articles/`);
     const data = response.data;
 
-    // Return the data as JSON
     res.status(200).json(data);
   } catch (error) {
     console.error("Error fetching data:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
-// Studying new fetch method
-// Update
