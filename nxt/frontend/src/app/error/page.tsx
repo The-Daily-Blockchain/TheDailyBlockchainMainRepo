219390be90e11c27516dedab2ea8c5e58d4c1ce5
Dexter <dexter.lente18@gmail.com>
2024-02-02 18:27:29 +0800

    add Error component

---
+++ nxt/frontend/src/app/error/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import Error from "../error";
+
+const page = () => {
+  return (
+    <div className="h-screen">
+      <Error />
+    </div>
+  );
+};
+
+export default page;
