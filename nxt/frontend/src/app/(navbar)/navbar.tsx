c08b42876dab86dd9bf1b9890c5494f52f4cb8c2
Dexter <dexter.lente18@gmail.com>
2024-02-07 14:49:23 +0800

    fix fix fix

--- nxt/frontend/src/app/(navbar)/navbar.tsx
+++ nxt/frontend/src/app/(navbar)/navbar.tsx
@@ -16,6 +16,7 @@ const NavBar: React.FC = () => {
           style={{ cursor: "pointer" }}
         >
           <Image
+            className="hover:opacity-80"
             src="/Daily.png"
             alt="Daily Blockchain Ph"
             width={700}
@@ -24,16 +25,41 @@ const NavBar: React.FC = () => {
         </div>
       </div>
       <div className="flex justify-center border-double border-b-4 border-[#000] bg-[#FFFFFF] space-x-16 py-3 text-[12px]">
-        <div style={{ cursor: "pointer" }}> About</div>
-        <div style={{ cursor: "pointer" }}> Top News</div>
-        <div style={{ cursor: "pointer" }}> Crypto101</div>
         <div
+          className="no-underline hover:underline hover:opacity-60"
+          style={{ cursor: "pointer" }}
+        >
+          {" "}
+          About
+        </div>
+        <div
+          className="no-underline hover:underline hover:opacity-60"
+          style={{ cursor: "pointer" }}
+        >
+          {" "}
+          Top News
+        </div>
+        <div
+          className="no-underline hover:underline hover:opacity-60"
+          style={{ cursor: "pointer" }}
+        >
+          {" "}
+          Crypto101
+        </div>
+        <div
+          className="no-underline hover:underline hover:opacity-60"
           style={{ cursor: "pointer" }}
           onClick={() => router.push(`/liveprices`)}
         >
           Live Prices
         </div>
-        <div style={{ cursor: "pointer" }}> Donate Us</div>
+        <div
+          className="no-underline hover:underline hover:opacity-60"
+          style={{ cursor: "pointer" }}
+        >
+          {" "}
+          Donate Us
+        </div>
       </div>
       <MarQuee />
     </div>
