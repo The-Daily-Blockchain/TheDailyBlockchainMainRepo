ce7d00237ba80afa33ca2a8b3bb4d693f8a3662d
Dexter <dexter.lente18@gmail.com>
2024-02-12 18:19:14 +0800

    add pagination on one page

--- nxt/frontend/src/app/crypto101/page.tsx
+++ nxt/frontend/src/app/crypto101/page.tsx
@@ -3,13 +3,16 @@ import React from "react";
 import BodyList from "../(mainbody)/BodyList";
 import useSWR from "swr";
 import { fetcher } from "../(components)/utils/fetcher";
+import Pagination from "../(mainbody)/pagination";
 
 const Page = () => {
   const title = "Crypto 101";
   const { data, isLoading, error } = useSWR("/api/post", fetcher);
+  const apiEndpoint = "/api/post";
   return (
     <div>
       <BodyList data={data} isLoading={isLoading} error={error} title={title} />
+      <Pagination apiEndpoint={apiEndpoint} />
     </div>
   );
 };
