eebe9ce6523a038dce62835446493c93446f402d
Dexter <dexter.lente18@gmail.com>
2024-02-15 17:39:44 +0800

    fix 101 thinking new features

--- nxt/frontend/src/app/crypto101/page.tsx
+++ nxt/frontend/src/app/crypto101/page.tsx
@@ -4,13 +4,17 @@ import BodyList from "../(mainbody)/BodyList";
 import useSWR from "swr";
 import { fetcher } from "../(components)/utils/fetcher";
 import Pagination from "../(mainbody)/pagination";
+import { useDataHandler } from "../(components)/utils/dataHandler";
 
 const Page = () => {
+  const { data, handleDataUpdate } = useDataHandler();
   const title = "Crypto 101";
-  const { data, isLoading, error } = useSWR("/api/post", fetcher);
+  const apiEndpoint = "/api/post";
+  const { isLoading, error } = useSWR(`${apiEndpoint}`);
   return (
     <div>
       <BodyList data={data} isLoading={isLoading} error={error} title={title} />
+      <Pagination apiEndpoint={apiEndpoint} onDataUpdate={handleDataUpdate} />
     </div>
   );
 };
