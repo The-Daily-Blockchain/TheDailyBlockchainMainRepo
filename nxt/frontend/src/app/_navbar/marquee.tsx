da2c551933d48b80c4534451bed0b14155eea2fa
Dexter <dexter.lente18@gmail.com>
2024-02-24 15:13:05 +0800

    marquee with mobile size

--- nxt/frontend/src/app/_navbar/marquee.tsx
+++ nxt/frontend/src/app/_navbar/marquee.tsx
@@ -46,41 +46,82 @@ const MarQuee = () => {
   if (!crypto) return <Skeleton />;
   return (
     <Marquee>
-      <div className="flex border-b-4 border-[#000]">
-        {crypto.map((coin) => (
-          <div
-            className="mr-3 flex border-solid border-black border-r-2 pr-2"
-            key={coin.id}
-          >
-            <img
-              src={coin.image}
-              alt={coin.name}
-              className="w-8 h-8 mt-2 mr-2 rounded-full"
-            />
+      <div className="hidden sm:block">
+        <div className="flex border-b-4 border-[#000]">
+          {crypto.map((coin) => (
+            <div
+              className="mr-3 flex border-solid border-black border-r-2 pr-2"
+              key={coin.id}
+            >
+              <img
+                src={coin.image}
+                alt={coin.name}
+                className="w-8 h-8 mt-2 mr-2 rounded-full"
+              />
 
-            <div className="grid grid-cols-1 mr-2">
-              <span> {coin.name}</span>
-              <span>{coin.symbol.toUpperCase()}</span>
+              <div className="grid grid-cols-1 mr-2">
+                <span> {coin.name}</span>
+                <span>{coin.symbol.toUpperCase()}</span>
+              </div>
+              <div className="grid grid-cols-1">
+                ₱{coin.current_price.toLocaleString("en-US")}
+                <p
+                  className={`justify-self-center px-1 ${
+                    typeof coin.price_change_percentage_24h === "number"
+                      ? coin.price_change_percentage_24h < 0
+                        ? "text-red-500"
+                        : "text-green-500"
+                      : "N/A"
+                  }`}
+                >
+                  {typeof coin.price_change_percentage_24h === "number"
+                    ? coin.price_change_percentage_24h.toFixed(2)
+                    : "N/A"}
+                  %
+                </p>
+              </div>
             </div>
-            <div className="grid grid-cols-1">
-              ₱{coin.current_price.toLocaleString("en-US")}
-              <p
-                className={`justify-self-center px-1 ${
-                  typeof coin.price_change_percentage_24h === "number"
-                    ? coin.price_change_percentage_24h < 0
-                      ? "text-red-500"
-                      : "text-green-500"
-                    : "N/A"
-                }`}
-              >
-                {typeof coin.price_change_percentage_24h === "number"
-                  ? coin.price_change_percentage_24h.toFixed(2)
-                  : "N/A"}
-                %
-              </p>
+          ))}
+        </div>
+      </div>
+      {/* mobile */}
+      <div className="sm:hidden">
+        <div className="flex border-b-2 border-[#000]">
+          {crypto.map((coin) => (
+            <div
+              className="mr-2 flex border-solid border-black border-r-2 pr-2"
+              key={coin.id}
+            >
+              <img
+                src={coin.image}
+                alt={coin.name}
+                className="w-4 h-4 mt-2 mr-2 rounded-full"
+              />
+
+              <div className="grid grid-cols-1 mr-2">
+                <span className="text-[10px]"> {coin.name}</span>
+                <span className="text-[10px]">{coin.symbol.toUpperCase()}</span>
+              </div>
+              <div className="grid grid-cols-1 text-[10px]">
+                ₱{coin.current_price.toLocaleString("en-US")}
+                <p
+                  className={`justify-self-center px-1 ${
+                    typeof coin.price_change_percentage_24h === "number"
+                      ? coin.price_change_percentage_24h < 0
+                        ? "text-red-500"
+                        : "text-green-500"
+                      : "N/A"
+                  }`}
+                >
+                  {typeof coin.price_change_percentage_24h === "number"
+                    ? coin.price_change_percentage_24h.toFixed(2)
+                    : "N/A"}
+                  %
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </Marquee>
   );
