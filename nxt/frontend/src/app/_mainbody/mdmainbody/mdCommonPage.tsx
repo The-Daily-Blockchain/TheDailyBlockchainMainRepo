1e6d259554e82f726f16bab1771944a0c4c1ca00
Dexter <dexter.lente18@gmail.com>
2024-02-26 16:18:43 +0800

    make all common page work

--- nxt/frontend/src/app/_mainbody/mdmainbody/mdCommonPage.tsx
+++ nxt/frontend/src/app/_mainbody/mdmainbody/mdCommonPage.tsx
@@ -1,10 +1,31 @@
 import React from "react";
+import parse from "html-react-parser";
+import { formatDate } from "@/app/_components/utils/formattingData";
 
 interface Props {
   payload: any;
 }
 const MdCommonPage = ({ payload }: Props) => {
-  return <div>mdCommonPage</div>;
+  return (
+    <div className="h-screen mx-6 mt-10 mb-10 overflow-y-hidden">
+      <div>
+        <div className="mb-10 font-bold text-lg">
+          {payload?.title || payload?.title_post}
+        </div>
+        <div className="text-sm">
+          By: {payload?.author?.first_name || payload?.author_post?.first_name}{" "}
+          {payload?.author?.last_name || payload?.author_post?.last_name}
+        </div>
+
+        <div className="mb-10 text-xs">
+          {formatDate(payload?.time_created || payload?.time_created_post)}
+        </div>
+        <div className="mb-10 text-sm">
+          {parse((payload?.content || "") + (payload?.content_post || ""))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MdCommonPage;
--- nxt/frontend/src/app/_mainbody/mobilemainbody/mobileCommonPage.tsx
+++ nxt/frontend/src/app/_mainbody/mobilemainbody/mobileCommonPage.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import parse from "html-react-parser";
+import { formatDate } from "@/app/_components/utils/formattingData";
 
 interface Props {
   payload: any;
 }
 
 const MobileCommonPage = ({ payload }: Props) => {
-  return <div>MobileCommonPage</div>;
+  return (
+    <div className="mx-2 mt-10 mb-10">
+      <div>
+        <div className="pb-10 mb- 20 font-bold text-lg">
+          {payload?.title || payload?.title_post}
+        </div>
+        <div className="text-xs">
+          By: {payload?.author?.first_name || payload?.author_post?.first_name}{" "}
+          {payload?.author?.last_name || payload?.author_post?.last_name}
+        </div>
+
+        <div className="mb-10 text-xs">
+          {formatDate(payload?.time_created || payload?.time_created_post)}
+        </div>
+        <div className="text-sm">
+          {parse((payload?.content || "") + (payload?.content_post || ""))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MobileCommonPage;
