1f5943e4e85ff8f38abb6b8d57031d600186d7f0
Dexterlente <dexter.lente18@gmail.com>
2024-03-05 02:07:00 +0800

    use swr

--- nxt/frontend/src/app/(tabs)/liveprices/new/page.tsx
+++ nxt/frontend/src/app/(tabs)/liveprices/new/page.tsx
@@ -1,48 +1,45 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { fetcher } from "@/app/_components/utils/fetcher";
+import useSWR from "swr";
+import Loader from "@/app/loader";
+import Error from "@/app/error";
 
 // ETH SOL XRP ADA DODGE SHIB AVAX DOT TRX LINK MATIC UNI TON LTC
 
 const Page = () => {
-  const [tickerData, setTickerData] = useState<Array<{ symbol: string }>>([]);
-  useEffect(() => {
-    const fetchTickerData = async () => {
-      try {
-        const binanceBaseUrl = "https://api.binance.com";
-        const query = encodeURIComponent(
-          JSON.stringify([
-            "BTCUSDT",
-            "ETHUSDT",
-            "BNBUSDT",
-            "SOLUSDT",
-            "XRPUSDT",
-            "ADAUSDT",
-            "DOGEUSDT",
-            "SHIBUSDT",
-            "AVAXUSDT",
-            "DOTUSDT",
-            "TRXUSDT",
-            "LINKUSDT",
-            "MATICUSDT",
-            "UNIUSDT",
-            "LTCUSDT",
-          ])
-        );
-        // "%5B%22BTCUSDT%22,%22ETHUSDT%22,%22BNBUSDT%22,%22SOLUSDT%22,%22XRPUSDT%22,%22ADAUSDT%22,%22DOGEUSDT%22,%22SHIBUSDT%22,%22AVAXUSDT%22,%22DOTUSDT%22,%22TRXUSDT%22,%22LINKUSDT%22,%22MATICUSDT%22,%22UNIUSDT%22,%22LTCUSDT%22%5D";
-        const apiEndpoint = `${binanceBaseUrl}/api/v3/ticker/24hr?symbols=${query}&type=MINI`;
+  const binanceBaseUrl = "https://api.binance.com";
+  const query = encodeURIComponent(
+    JSON.stringify([
+      "BTCUSDT",
+      "ETHUSDT",
+      "BNBUSDT",
+      "SOLUSDT",
+      "XRPUSDT",
+      "ADAUSDT",
+      "DOGEUSDT",
+      "SHIBUSDT",
+      "AVAXUSDT",
+      "DOTUSDT",
+      "TRXUSDT",
+      "LINKUSDT",
+      "MATICUSDT",
+      "UNIUSDT",
+      "LTCUSDT",
+    ])
+  );
 
-        const response = await axios.get(apiEndpoint);
+  const apiEndpoint = `${binanceBaseUrl}/api/v3/ticker/24hr?symbols=${query}&type=MINI`;
 
-        setTickerData(response.data);
-      } catch (error) {}
-    };
-    fetchTickerData();
-  }, []);
+  const { data, isLoading, error } = useSWR(apiEndpoint, fetcher);
+
+  if (isLoading) return <Loader />;
+  if (error) return <Error />;
 
   return (
     <div>
-      {tickerData.map((item, index) => (
+      {data?.map?.((item: any, index: any) => (
         <div key={index}>
           <p>{item.symbol}</p>
         </div>
