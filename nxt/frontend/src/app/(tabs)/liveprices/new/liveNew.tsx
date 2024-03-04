f8d61dfa450a10ce9987482edeb2d817ea9ecc24
Dexterlente <dexter.lente18@gmail.com>
2024-03-04 23:40:40 +0800

    crypto price

---
+++ nxt/frontend/src/app/(tabs)/liveprices/new/liveNew.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const binanceBaseUrl = "https://api.binance.com";
+
+// sample https://api.binance.com/api/v3/ticker/24hr?symbols=%5B%22BTCUSDT%22,%22BNBUSDT%22%5D&type=MINI
+
+// ETH SOL XRP ADA DODGE SHIB AVAX DOT TRX LINK MATIC UNI TON LTC
+const liveNew = () => {
+  return <div>liveNew</div>;
+};
+
+export default liveNew;
