bde43f454c73ca8755e70b04f4bd8080f4d8e91f
Dexter <dexter.lente18@gmail.com>
2024-03-05 15:59:41 +0800

    fix query

--- nxt/frontend/src/app/(tabs)/liveprices/new/page.tsx
+++ nxt/frontend/src/app/(tabs)/liveprices/new/page.tsx
@@ -1,49 +1,95 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { fetcher } from "@/app/_components/utils/fetcher";
-import useSWR from "swr";
-import Loader from "@/app/loader";
-import Error from "@/app/error";
 
-// ETH SOL XRP ADA DODGE SHIB AVAX DOT TRX LINK MATIC UNI TON LTC
+type TickerData = {
+  e: string;
+  E: number;
+  s: string;
+  c: string;
+  o: string;
+  h: string;
+  l: string;
+  v: string;
+  q: string;
+};
 
 const Page = () => {
-  const binanceBaseUrl = "https://api.binance.com";
-  const query = encodeURIComponent(
-    JSON.stringify([
-      "BTCUSDT",
-      "ETHUSDT",
-      "BNBUSDT",
-      "SOLUSDT",
-      "XRPUSDT",
-      "ADAUSDT",
-      "DOGEUSDT",
-      "SHIBUSDT",
-      "AVAXUSDT",
-      "DOTUSDT",
-      "TRXUSDT",
-      "LINKUSDT",
-      "MATICUSDT",
-      "UNIUSDT",
-      "LTCUSDT",
-    ])
-  );
+  const [tickerData, setTickerData] = useState<TickerData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const apiEndpoint = `${binanceBaseUrl}/api/v3/ticker/24hr?symbols=${query}&type=MINI`;
+  useEffect(() => {
+    let socket: WebSocket | null = null;
 
-  const { data, isLoading, error } = useSWR(apiEndpoint, fetcher);
+    const query =
+      "btcusdt@miniTicker/ethusdt@miniTicker/bnbusdt@miniTicker/solusdt@miniTicker/xrpusdt@miniTicker/adausdt@miniTicker/dogeusdt@miniTicker/shibusdt@miniTicker/avaxusdt@miniTicker/dotusdt@miniTicker/trxusdt@miniTicker/linkusdt@miniTicker/maticusdt@miniTicker/uniusdt@miniTicker/ltcusdt@miniTicker";
+    const connectWebSocket = () => {
+      socket = new WebSocket(
+        `wss://stream.binance.com:9443/stream?streams=${query}`
+      );
 
-  if (isLoading) return <Loader />;
-  if (error) return <Error />;
+      socket.onmessage = (event) => {
+        const newData = JSON.parse(event.data);
+        const pair = newData.stream.split("@")[0];
+        console.log(pair);
+        setTickerData((prevData) => ({
+          ...prevData,
+          [pair]: newData.data,
+        }));
+
+        setIsLoading(false);
+      };
+
+      socket.onclose = () => {
+        // Automatically reconnect after 24 hours
+        setTimeout(connectWebSocket, 24 * 60 * 60 * 1000);
+      };
+    };
+
+    connectWebSocket();
+
+    return () => {
+      if (socket) {
+        socket.close();
+      }
+    };
+  }, []);
 
   return (
     <div>
-      {data?.map?.((item: any, index: any) => (
-        <div key={index}>
-          <p>{item.symbol}</p>
-        </div>
-      ))}
+      <h1>Binance Mini Ticker Stream</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>Close Price</th>
+            <th>Open Price</th>
+            <th>Highest Price</th>
+            <th>Lowest Price</th>
+            <th>Volume (Base Asset)</th>
+            <th>Volume (Quote Asset)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(tickerData).map((pair: any) => (
+            <tr key={pair}>
+              {/* {Object.entries(tickerData[pair]).map(
+                ([key, value]: [string, any]) => (
+                  <td key={key}>{value}</td>
+                )
+              )} */}
+              <td>{tickerData[pair].e}</td>
+              <td>{tickerData[pair].E}</td>
+              <td>{tickerData[pair].s}</td>
+              <td>{tickerData[pair].c}</td>
+              <td>{tickerData[pair].o}</td>
+              <td>{tickerData[pair].h}</td>
+              <td>{tickerData[pair].l}</td>
+              <td>{tickerData[pair].v}</td>
+              <td>{tickerData[pair].q}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
