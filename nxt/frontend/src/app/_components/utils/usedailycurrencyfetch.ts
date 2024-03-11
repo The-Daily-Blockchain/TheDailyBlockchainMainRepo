d83d708636e3be2efe8863d15f2a28b5e8fd91fa
Dexterlente <dexter.lente18@gmail.com>
2024-03-12 04:09:37 +0800

    trigger only once a day

--- nxt/frontend/src/app/_components/utils/usedailycurrencyfetch.ts
+++ nxt/frontend/src/app/_components/utils/usedailycurrencyfetch.ts
@@ -6,14 +6,31 @@ import { useDebouncedValue } from "./usedebouncevalue";
 export const useDailyCurrencyFetch = () => {
   const intervalRef = useRef(null);
 
-  const currency = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${api_key_currency}&symbols=PHP`;
+  const currencyUrl = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${api_key_currency}&symbols=PHP`;
 
-  const debounceCurrency = useDebouncedValue(currency, 86400000);
+  const debounceCurrency = useDebouncedValue(currencyUrl, 86400000);
+
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(debounceCurrency);
-      intervalRef.current = response.data.rates.PHP;
+      const lastFetchedDate = localStorage.getItem("currencyLastFetched");
+      const currentDate = new Date().toISOString().slice(0, 10);
+
+      if (lastFetchedDate !== currentDate) {
+        try {
+          const response = await axios.get(debounceCurrency);
+          intervalRef.current = response.data.rates.PHP;
+          localStorage.setItem("currencyLastFetched", currentDate);
+        } catch (error) {
+          console.error("Error fetching currency data:", error);
+        }
+      } else {
+        const cachedData = localStorage.getItem("cachedCurrencyData");
+        if (cachedData) {
+          intervalRef.current = JSON.parse(cachedData);
+        }
+      }
     };
+
     fetchData();
   }, [debounceCurrency]);
 
