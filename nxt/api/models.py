62ca6d4e0edb40b0f347621d7b78640795d47cd6
Dexter <dexter.lente18@gmail.com>
2024-04-08 18:38:36 +0800

    fixed

--- nxt/api/models.py
+++ nxt/api/models.py
@@ -37,9 +37,6 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
-# upload views needed
-# urlfield for article need to add logic for the upload
-# wards so url field right?
 
 
 class Post(models.Model):
