733d7361e7c1b2436075d8d5439d7bc7ea4eb0a2
Dexter <dexter.lente18@gmail.com>
2024-08-17 13:35:34 +0800

    updaqte

--- nxt/nxt/settings.py
+++ nxt/nxt/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 # CORS_ALLOWED_ORIGINS = [
 #     # "http://localhost:3000",
